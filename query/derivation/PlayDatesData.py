9f454f13779d2dc501bbe7291aa2a6d7552183b7
andyccoge <andyccoge@gmail.com>
2025-09-02 13:22:50 +0800

    調整撈取更多資料的打球日資料API 1.場地僅計算比上場(type=1)的數量 2.以子表方式join來增加效能

--- query/derivation/PlayDatesData.py
+++ query/derivation/PlayDatesData.py
@@ -11,26 +11,42 @@ class PlayDatesData(PlayDate):
 
   # 取得報名紀錄
   def get_data(self, where={}):
-    # 設定撈取欄位&表
-    db_query = select(
-                self._cols.id,
-                self._cols.location,
-                self._cols.note,
-                func.date_format(self._cols.datetime, "%Y-%m-%d %H:%i").label("datetime"),
-                func.date_format(self._cols.datetime2, "%Y-%m-%d %H:%i").label("datetime2"),
-                func.count(distinct(self._courts.c.id)).label("count_courts"),
-                func.count(distinct(self._reservations.c.id)).label("count_reservations"),
-              ).select_from(
-                self._table
-                .join(self._courts, self._courts.c.play_date_id == self._cols.id, isouter=True)
-                .join(self._reservations, self._reservations.c.play_date_id == self._cols.id, isouter=True)
-              ).group_by(
-                self._cols.id,
-                self._cols.location,
-                self._cols.note,
-                self._cols.datetime,
-                self._cols.datetime2,
-              )
+    # 子查詢：計算 courts type=1 的數量
+    courts_subq = (
+        select(
+            self._courts.c.play_date_id,
+            func.count(self._courts.c.id).label("count_courts_type1")
+        )
+        .where(self._courts.c.type == 1)
+        .group_by(self._courts.c.play_date_id)
+    ).subquery()
+
+    # 子查詢：計算 reservations 數量
+    reservations_subq = (
+        select(
+            self._reservations.c.play_date_id,
+            func.count(self._reservations.c.id).label("count_reservations")
+        )
+        .group_by(self._reservations.c.play_date_id)
+    ).subquery()
+
+    # 主查詢：以 _table 為基準
+    db_query = (
+      select(
+          self._cols.id,
+          self._cols.location,
+          self._cols.note,
+          func.date_format(self._cols.datetime, "%Y-%m-%d %H:%i").label("datetime"),
+          func.date_format(self._cols.datetime2, "%Y-%m-%d %H:%i").label("datetime2"),
+          func.coalesce(courts_subq.c.count_courts_type1, 0).label("count_courts"),
+          func.coalesce(reservations_subq.c.count_reservations, 0).label("count_reservations"),
+      )
+      .select_from(
+          self._table
+          .outerjoin(courts_subq, courts_subq.c.play_date_id == self._cols.id)
+          .outerjoin(reservations_subq, reservations_subq.c.play_date_id == self._cols.id)
+      )
+    )
 
     # 設定篩選條件
     [db_query, *_] = self.deal_where_query(db_query, where)
